aec0f1f303d4499f400742cf0d03fd246d9f3d58
Tejaswini Khairnar <117677072+Tejaswini1397@users.noreply.github.com>
2023-11-22 19:41:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,15 +1,19 @@
 //your JS code here. If required.
 function secondHighest(arr) {
 			//Write your code here
-		 if (arr.length < 2) {
-        return "Array should have at least two elements.";
+		let firstMax = -Infinity;
+    let secondMax = -Infinity;
+
+    for (let num of arr) {
+        if (num > firstMax) {
+            secondMax = firstMax;
+            firstMax = num;
+        } else if (num > secondMax && num < firstMax) {
+            secondMax = num;
+        }
     }
 
-    // Sort the array in descending order
-    arr.sort((a, b) => b - a);
-
-    // Return the second element (second-highest)
-    return arr[1];
+    return secondMax;
 		}
 		function Main() {
 			var n = prompt("Enter the number of elements");
